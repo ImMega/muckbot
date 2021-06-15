339ea8e585b651b330be44fba0b07b72438954e2
ImMega <72713009+ImMega@users.noreply.github.com>
2021-06-15 14:12:18 +0200

    muck

---
+++ main.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+client.once("ready", () => {
+    console.log(`${client.user.username}` + " is online");
+});
+
+client.on("message", message =>{
+    if (!message.author.bot) {
+        const muck = message.content === "muck" || message.content === "Muck" || message.content === "mUck" || message.content === "muCk" || message.content === "mucK" || message.content === "mUCK" || message.content === "MuCK" || message.content === "MUcK" || message.content === "MUCk" || message.content === "MUck" || message.content === "mUCk" || message.content === "muCK" || message.content === "MucK"
+        const MUCK = message.content === "MUCK"
+        if (muck) {
+            message.channel.send("muck");
+        } else if (MUCK) {
+            message.channel.send("MUCK");
+        }
+    }
+})
+
+
+
+client.login(process.env.token);
